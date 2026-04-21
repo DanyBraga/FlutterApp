5eb7f2d77f38be0c1b2fa4ee0bafb7e669e8f768
daniely.s.braga <daniely.s.braga@gmail.com>
2026-04-21 00:38:16 +0000

    Semana 8

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'components.dart';
 
 void main() {
   runApp(const MyApp());
@@ -66,9 +67,10 @@ class _MyHomePageState extends State<MyHomePage> {
       _counter++;
     });
   }
-    void  _decrementCounter() {
+
+  void _decrementCounter() {
     setState(() {
-       _counter--;
+      _counter--;
     });
   }
 
@@ -90,6 +92,39 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
+      drawer: Drawer(
+        child: SafeArea(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                ),
+                child: Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Text(
+                    'Menu',
+                    style: Theme.of(context).textTheme.headlineSmall,
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: const Icon(Icons.pages),
+                title: const Text('Componentes'),
+                onTap: () {
+                  Navigator.pop(context); // fecha o drawer
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => const ComponentsPage()),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
@@ -114,13 +149,11 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
-          TextButton(onPressed: _incrementCounter, child: Text('Increment')),
-          TextButton(onPressed: _decrementCounter, child: Text('Decrement')),
-
+            TextButton(onPressed: _incrementCounter, child: Text('Increment')),
+            TextButton(onPressed: _decrementCounter, child: Text('Decrement')),
           ],
         ),
       ),
-
     );
   }
 }
